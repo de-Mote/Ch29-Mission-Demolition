92109fc637c0339955b4674342643ebd194e7936
PJ Rosa <demotednyk@gmail.com>
2016-02-25 01:21:17 -0500

    [peter-rosa] finished!

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -1,36 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
+public class FollowCam : MonoBehaviour {
+	static public FollowCam S; // a FollowCam Singleton
 
-public class FollowCam : MonoBehaviour
-{
-    static public FollowCam S;
-    public bool _____________________________;
-    public GameObject poi;
-    public float camZ;
-    public Vector2 minXY;
+	// fields set in the Unity Inspector pane
+	public float easing = 0.01f;
+	public Vector2 minXY;
+	public bool _____________________________;
 
-    public float easing = 0.05f;
-    void Awake()
-    {
-        S = this;
-        camZ = this.transform.position.z;
-    }
-    void Update()
-    {
-        // if there's only one line following an if, it doesn't need braces
-        if (poi == null) return; // return if there is no poi
-        // Get the position of the poi
-        Vector3 destination = poi.transform.position;
-        // Limit the X & Y to minimum values
-        destination.x = Mathf.Max(minXY.x, destination.x);
-        destination.y = Mathf.Max(minXY.y, destination.y);
-        // Interpolate from the current Camera position toward destination
-        destination = Vector3.Lerp(transform.position, destination, easing);
-        // Retain a destination.z of camZ
-        destination.z = camZ;
-        // Set the camera to the destination
-        transform.position = destination;
-        // Set the orthographicSize of the Camera to keep Ground in view
-        this.GetComponent<Camera>().orthographicSize = destination.y + 10;
-    }
+	// fields set dynamically
+	public GameObject poi; // The Point of Interest
+	public float camZ; // The desired Z pos of the Camera
+
+	void Awake() {
+		S = this;
+		camZ = this.transform.position.z;
+	}
+
+	void FixedUpdate () {
+		Vector3 destination;
+		// If there is no poi, return to P:[0,0,0]
+		if (poi == null) {
+			destination = Vector3.zero;
+		} else {
+			// Get the position of the poi
+			destination = poi.transform.position;
+			// If poi is a Projectile, check to see if it's at rest
+			if (poi.tag == "Projectile") {
+				// if it is sleeping (that is, not moving)
+				if ( poi.GetComponent<Rigidbody>().IsSleeping() ) {
+					// return to default view
+					poi = null;
+					// in the next update
+					return;
+				}
+			}
+		}
+		// Limit the X & Y to minimum values
+		destination.x = Mathf.Max( minXY.x, destination.x );
+		destination.y = Mathf.Max( minXY.y, destination.y );
+		// Interpolate from the current Camera position towards destination
+		destination = Vector3.Lerp( transform.position, destination, easing );
+		// Retain a destination.z of camZ
+		destination.z = camZ;
+		// Set the Camera to the destination
+		transform.position = destination;
+		// Set the orthographicSize of the Camera to keep Ground in view
+		this.GetComponent<Camera>().orthographicSize = destination.y + 10;
+	}
 }
